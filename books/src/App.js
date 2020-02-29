767a5f99a95f2405381326dc6f3ef453e749d75f
AmanKumarSingh01 <imh10012.16@bitmesra.ac.in>
2020-02-29 13:01:00 +0530

    Some redux implementation

--- books/src/App.js
+++ books/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Provider} from 'react-redux'
+import store from './Store'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import {BrowserRouter as Router ,Route} from 'react-router-dom'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <Route exact path="/" component= {Landing}/>
+            <div>
+              <Route exact path="/login" component= {Login}/>
+              <Route exact path="/register" component= {Register}/>
+            </div>
+        </div>
+      </Router>
+      </Provider>
+      
+    )
+  }
 }
 
 export default App;
